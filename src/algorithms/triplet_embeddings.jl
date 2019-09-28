63a046ab86f5b2c80d4ffe2abfe23507a767604d
Karel Mundnich <mundnich@usc.edu>
2019-09-28 15:00:07 -0700

    Added kwargs to complete for triplet embeddings

--- src/algorithms/triplet_embeddings.jl
+++ src/algorithms/triplet_embeddings.jl
@@ -1,9 +1,9 @@
-function complete(D::MaskedEuclideanDistanceMatrix{T}, loss::TripletEmbeddings.AbstractLoss, d::Int) where T <: Real
+function complete(D::MaskedEuclideanDistanceMatrix{T}, loss::TripletEmbeddings.AbstractLoss, d::Int; kwargs...) where T <: Real
     @assert d ≥ 1 "d must be ≥ 1"
 
     triplets = Triplets(D)
-    X = Embedding(d, size(D,1))
-    _ = fit!(loss, triplets, X)
+    X = Embedding(d, size(D, 1))
+    _ = fit!(loss, triplets, X; kwargs...)
 
     D̂ = EuclideanDistanceMatrix(X)
     s = mean(filter(!isnan, D./D̂))    
